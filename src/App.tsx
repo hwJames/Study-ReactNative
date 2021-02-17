ed92c97eddaa958b2c60d004bc8d2c88c8caad09
KJH <kjhg5852@naver.com>
2021-02-17 17:58:14 +0900

    navigation v5 라이브러리 적용

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,23 @@
  * @format
  */
 
+import 'react-native-gesture-handler';
 import React, {useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import Header from './Header';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const App = () => {
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+const MainScreen = () => {
   useEffect(() => {
     setTimeout(() => {
       SplashScreen.hide();
@@ -27,12 +38,16 @@ const App = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={MainScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
